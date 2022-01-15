6b11a7e0db999a88ea8d0c253065c80c3e3e7942
youngseoKim-kr <ysk5754@naver.com>
2022-01-15 18:31:32 +0900

    complete

--- src/main.js
+++ src/main.js
@@ -8,12 +8,44 @@ function loadItems() {
         .then(json => json.items);
 }
 
+//아이템을 가져와서 보여준다.
+function displayItems(items){
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+//li태그를 만들어서 넣어줌
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class=="item__description">${item.gender}, ${item.size}</span>
+      </li>
+    `;
+}
+
+function onButtonClick(event, items) {
+    const datset = event.target.dataset;
+    const key = datset.key;
+    const value = datset.value;
+    // button 전체 범위를 가져오기 때문에 버튼을 안누르면 값이 없다.
+    if(value == null || key == null) return;
+
+    displayItems(items.filter(item => item[key] === value));
+}
+
+//버튼 클릭시 원하는 정보만 출력
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const button = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    button.addEventListener('click', event => onButtonClick(event, items));
+}
 
 //main
 loadItems()
 .then(items => {
-    console.log(items);
-    // displayItems(items);
-    // setEventListeners(items)
+    displayItems(items);
+    setEventListeners(items)
 })
 .catch(console.log);
